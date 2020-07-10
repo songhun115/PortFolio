9fd35786acd4a410083fa268f70072e47bad5807
hoho77113@daum.net <hoho77131@daum.net>
2020-07-10 16:38:27 +0900

    Javascript Home Contact Button

--- main.js
+++ main.js
@@ -11,14 +11,25 @@ document.addEventListener("scroll", () => {
   }
 });
 
+//Click to move to the menu item position
 const navbarMenu = document.querySelector(".navbar__menu");
+
 navbarMenu.addEventListener("click", () => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link); // <- 이 아래 함수를 불러다 아이디만 적어주면 그 아이디위치로 이동
 });
+
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact"); //<- 이 아래 함수를 불러다 아이디만 적어주면 그 아이디위치로 이동
+});
+
+// 화면을 선택한 section의 위치로 이동 시키는 함수
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
