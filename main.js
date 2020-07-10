3af66649b19675047bfd01c1f7837b5536db0a42
hoho77113@daum.net <hoho77131@daum.net>
2020-07-10 17:55:06 +0900

    Javascript UpButton

--- main.js
+++ main.js
@@ -12,18 +12,20 @@ document.addEventListener("scroll", () => {
 });
 
 //Click to move to the menu item position
-const navbarMenu = document.querySelector(".navbar__menu");
+const navbarMenu = document.querySelector(".navbar__menu"); //<- 변수 선언 navbarMenu는 navbar__menu이다
 
 navbarMenu.addEventListener("click", () => {
+  //<- 클릭시 이벤트 발생
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
+
   scrollIntoView(link); // <- 이 아래 함수를 불러다 아이디만 적어주면 그 아이디위치로 이동
 });
 
-const homeContactBtn = document.querySelector(".home__contact");
+const homeContactBtn = document.querySelector(".home__contact"); // <- 변수 선언 homeContactBtn home__contact이다
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact"); //<- 이 아래 함수를 불러다 아이디만 적어주면 그 아이디위치로 이동
 });
@@ -33,3 +35,41 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+//Make home slowly fade to transparent as the indow scrolls down.home_contact
+// 스크롤이 내려가면 home에 있는 컨텐츠들이 점점 투명해짐
+const home = document.querySelector(".home__container"); // <-변수 선언
+const homeheight = home.getBoundingClientRect().height; // <- home의 높이를 구한다.
+document.addEventListener("scroll", () => {
+  // <- 스크롤이 내려가면 이벤트 발생
+  home.style.opacity = 1 - window.scrollY / homeheight; // <- 변수의 css를 1 - window.scrollY / homeheight 계산식으로 수정
+}); // 위의 계산식처럼 home__container의오퍼시티가 적용됨
+
+//스크롤을 내리면 버튼이 생기는 함수
+const upButton = document.querySelector("#up__Button");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeheight / 2) {
+    upButton.classList.add("visible");
+  } else {
+    upButton.classList.remove("visible");
+  }
+});
+
+// upButton을 클릭하면 홈으로 이동하는 함수
+upButton.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+/* 직접만든 JS코드 
+const upButton = document.querySelector(".up__Button");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    upButton.style.opacity = 1;
+  } else if (window.scrollY < navbarHeight) {
+    upButton.style.opacity = 0;
+  }
+});
+
+upButton.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+*/
